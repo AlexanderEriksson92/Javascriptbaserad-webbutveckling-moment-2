ae94c307f13089630192a445e02246c310f4872f
Alex <alexanderE1992@hotmail.com>
2023-11-29 12:27:29 +0100

    delete fungerar

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.listen(port, () => {
     console.log(`Servern körs på http://localhost:${port}`);
 });
-// Hämtar alla kurse
+// Hämtar alla kurser
 app.get('/courses', (req, res) => {
     console.log('GET /courses');
     res.json(courses);
@@ -28,16 +28,22 @@ app.get('/courses/:id', (req, res) => {
 });
 
 app.delete('/courses/:id', (req, res) => {
-    console.log('GET /courses/id');
-    const course = courses.find(c => c._id == req.params.id); 
+    try {
+        const course = courses.find(c => c._id == req.params.id); 
 
-    if (!course) {
-        return res.status(404).send('Kursen hittades inte.');
+        if (!course) {
+            return res.status(404).send('Kursen hittades inte.');
+        }
+        
+        const index = courses.indexOf(course);
+        courses.splice(index, 1);
+        setTimeout(() => {
+            res.send(`Kursen med id: ${req.params.id} är borttagen`);
+        }, 1000);
     }
-    
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-    
+    catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 
