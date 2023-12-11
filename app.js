bbf1627b882b0219b4dc74cf213bdf354491a1ac
Alex <alexanderE1992@hotmail.com>
2023-12-11 17:55:05 +0100

    post fungerar men fel id tilldelas

--- app.js
+++ app.js
@@ -3,49 +3,107 @@
 var express = require('express');
 var app = express();
 var port = 3000;
-const courses = require('./courses.json');
+
 app.use(express.json());
 app.use(express.static('views'));
 
-// 
+
+const mongoose = require('mongoose');
+
+// Anslut till databasen
+mongoose.connect('mongodb://127.0.0.1:27017/myCB')
+
+    .then(() => console.log('Ansluten till MongoDB'))
+    .catch(err => console.error('Kunde inte ansluta', err));
+
+
+// Schema 
+const courseSchema = new mongoose.Schema({
+    name: String,
+    courseId: String,
+    progression: String,
+    syllabus: String,
+    term: String,
+    id: Number
+});
+
+
+const Course = mongoose.model('Course', courseSchema);
+
 app.listen(port, () => {
     console.log(`Servern körs på http://localhost:${port}`);
 });
+
 // Hämtar alla kurser
-app.get('/courses', (req, res) => {
-    res.json(courses);
-}
-);
-// Hämtar en kurs med id
-app.get('/courses/:id', (req, res) => {
-    const course = courses.find(c => c._id == req.params.id); 
-
-    if (!course) {
-        return res.status(404).send('Kursen hittades inte.');
-    }
-
-    res.send(course);
-});
-
-// Hämtar en kurs med id och raderar den sedan med splice
-app.delete('/courses/:id', (req, res) => {
+app.get('/courses', async (req, res) => {
     try {
-        const course = courses.find(c => c._id == req.params.id); 
-
-        if (!course) {
-            return res.status(404).send('Kursen hittades inte.');
-        }
-        
-        const index = courses.indexOf(course);
-        courses.splice(index, 1);
-        
-            res.send(`Kursen med id: ${req.params.id} är borttagen`);
-       
+        let courses = await Course.find();
+        res.json(courses);
     }
     catch (error) {
         res.status(500).send(error);
     }
+}
+);
+// Hämtar en kurs med id
+app.get('/courses/:id', async (req, res) => {
+    const course = await Course.findById(req.params.id);
+    try {
+        if (!course) {
+            return res.status(404).send('Kursen hittades inte.');
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
+
+app.post('/courses', async (req, res) => {
+    
+    try {
+       
+        // Hittar det högsta idet i databasen och lägger till 1
+        const highestId = await Course.find().sort('-id').limit(1);
+        let newId = 1;
+
+        // Är det fler än 0 kurser i databasen så sätts id till +1
+        if (highestId.length > 0) {
+            newId = highestId[0].id + 1;
+        }
+      
+        let course = new Course({
+            name: req.body.name,
+            courseId: req.body.courseId,
+            progression: req.body.progression,
+            syllabus: req.body.syllabus,
+            term: req.body.term,
+            id: newId
+        });
+        
+        console.log(course);
+        course = await course.save();
+        res.send(course);
+    } catch (error) {
+        console.error("Error Details: ", error);
+        res.status(500).send(error);
+    }
+});
+// Hämtar en kurs med id och raderar den sedan med splice
+
+app.delete('/courses/:id', async (req, res) => {
+    try {
+        const course = await Course.findById(req.params.id);
+
+        if (!course) {
+            return res.status(404).send('Kursen hittades inte.');
+        }
+
+        await course.remove();
+        res.send(`Kursen med id: ${req.params.id} är borttagen`);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 
 
 
